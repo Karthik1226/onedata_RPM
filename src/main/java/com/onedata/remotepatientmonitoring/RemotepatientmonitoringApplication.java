c106a876c8d78c78789d734f87bc22bd26e0d32e
KarthikRaja <duskyrajj8@gmail.com>
2025-04-11 15:00:19 +0530

    First commit

---
+++ src/main/java/com/onedata/remotepatientmonitoring/RemotepatientmonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.onedata.remotepatientmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotepatientmonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemotepatientmonitoringApplication.class, args);
+	}
+
+}
